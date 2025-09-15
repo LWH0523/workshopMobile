a49be8ad7596a9d26dbb252812a6d3a112b99f39
Suai Wen <wongsw-wm22@student.tarc.edu.my>
2025-09-15 14:05:50 +0800

    change ui

--- lib/ui/update.dart
+++ lib/ui/update.dart
@@ -120,7 +120,9 @@ class _SetRoutePageState extends State<SetRoutePage> {
         centerTitle: true,
         leading: IconButton(
           icon: const Icon(Icons.arrow_back_ios, color: Colors.white),
-          onPressed: () {},
+          onPressed: () {
+            Navigator.pop(context);
+          },
         ),
       ),
       body: Column(
